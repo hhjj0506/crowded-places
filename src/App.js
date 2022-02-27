d6ec9edbbef5fc95303450fbfcc409a9029d6b3b
Hyeongjin Kim <hhjj0506@gmail.com>
2022-02-27 23:11:50 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from 'react';
+import { NaverMap, Marker } from 'react-naver-maps';
+import { RenderAfterNavermapsLoaded } from 'react-naver-maps'
+import { TextField } from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    return (
+      <body>
+        <div id='search'>
+            <TextField id="outlined-basic" label="" variant="outlined" />
+        </div>
+        <div id='map'>
+            <RenderAfterNavermapsLoaded	   // Render후 Navermap로드
+                    ncpClientId={'kaqq42ubze'} // 자신의 네이버 계정에서 발급받은 Client ID
+                    error={<p>Maps Load Error</p>}
+                    loading={<p>Maps Loading...</p>}
+                >
+                <NaverMap
+                    id="react-naver-maps-introduction"
+                    style={{ width: '70%', height: '70vh' }}
+                    center={{ lat:36.10249203600525, lng: 129.38981598415268 }}
+                >
+                    <Marker 
+                        position={{ lat:36.10249203600525, lng: 129.38981598415268 }}
+                        onClick={()=> {
+                            alert('한동대학교')
+                        }}
+                    />
+                </NaverMap>
+            </RenderAfterNavermapsLoaded>
+        </div>
+      </body>
+
+    )
 }
 
 export default App;
